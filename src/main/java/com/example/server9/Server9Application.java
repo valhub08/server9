89540ff5655abf67521ad80b99bd8f15cd1785de
valhub08 <jhstl1203@gmail.com>
2024-09-27 19:27:03 +0900

    first

---
+++ src/main/java/com/example/server9/Server9Application.java
@@ -0,0 +1,13 @@
+package com.example.server9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Server9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Server9Application.class, args);
+    }
+
+}
