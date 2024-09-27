056f97b3513315d0a2044b636a5e2eab90af1360
valhub08 <jhstl1203@gmail.com>
2024-09-27 21:59:13 +0900

    JoinMemberController build

--- src/main/java/com/example/server9/controller/JoinMemberController.java
+++ src/main/java/com/example/server9/controller/JoinMemberController.java
@@ -8,21 +8,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RequiredArgsConstructor
 @RestController
-public class MemberController {
+public class JoinMemberController {
 
     private final MemberService memberService;
 
-    // 회원가입 API
     @PostMapping("/join")
     public Long join(@Valid @RequestBody MemberDto memberDto) {
         return memberService.join(memberDto);
     }
 
-    // 로그인 API
-    @PostMapping("/login")
-    public String login(@RequestBody MemberDto memberDto) {
-        return memberService.login(memberDto);
-    }
 }
