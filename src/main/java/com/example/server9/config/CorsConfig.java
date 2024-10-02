1782c025ef04d981ce1b781cc05e3b3cf2014bf2
valhub08 <jhstl1203@gmail.com>
2024-10-02 12:25:31 +0900

    CorsConfig build

---
+++ src/main/java/com/example/server9/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.example.server9.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:8080");  // 허용할 도메인
+        config.addAllowedHeader("*");  // 모든 헤더 허용
+        config.addAllowedMethod("*");  // 모든 HTTP 메서드 허용
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
